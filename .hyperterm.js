faee1217f1cc9261d530f0751a6dde5b9144a33d
Justin Zimmerman <jzimmerman@redventures.com>
2016-07-28 08:40:26 -0400

    add hyperlinks plugin

--- .hyperterm.js
+++ .hyperterm.js
@@ -69,7 +69,8 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
-    'hyperterm-solarized-dark'
+    'hyperterm-solarized-dark',
+    'hyperlinks'
   ],
 
   // in development, you can create a directory under
